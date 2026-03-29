ac2b679bd71cd2e17824f1578168ca23523d7f3f
nexoventlabs-official <nexoventlabs@gmail.com>
2026-03-29 18:34:42 +0530

    fixed 2

--- backend/routes/flowImages.js
+++ backend/routes/flowImages.js
@@ -62,7 +62,12 @@ const FLOW_IMAGE_KEYS = [
   'flow_cart_banner',
   'flow_cart_place_order',
   'flow_cart_add_more',
-  'flow_cart_clear'
+  'flow_cart_clear',
+  'flow_order_actions_banner',
+  'flow_action_track',
+  'flow_action_cancel',
+  'flow_action_order_food',
+  'flow_action_main_menu'
 ];
 
 // Determine Cloudinary crop dimensions based on image type
